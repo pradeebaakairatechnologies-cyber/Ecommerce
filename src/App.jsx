0c698a9774f6866c211e49bccfa632fea90765c6
pradeebaakairatechnologies-cyber <pradeebaa.kairatechnologies@gmail.com>
2026-04-20 11:08:42 +0530

    Add TopHeader component and components folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import './App.css'
+import TopHeader from './components/TopHeader'
 
 function App() {
 
   return (
     <>
-      
+      <TopHeader />
     </>
   )
 }
---
+++ src/components/TopHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './TopHeader.css';
+
+const TopHeader = () => {
+    return (
+        <div className="top-header">
+            <p>FREE SHIPPING ABOVE RS.1500/- IN PAN INDIA</p>
+        </div>
+    );
+};
+
+export default TopHeader;
